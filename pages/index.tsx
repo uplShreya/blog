ca80eee18563bdfc2319858768ca6d9d28d3113c
uplShreya <shreya.saha@utplco.com>
2022-11-03 11:18:14 +0530

    console

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,39 @@
+import Link from 'next/link';
 import { useEffect } from 'react'
 
+async function getPosts() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_apiurl}/content/posts/?key=${process.env.NEXT_PUBLIC_apiKEY}`)
+    .then(res => {
+      return res.json();
+    })
+  const posts = res.posts
+  return posts
+}
 
-export default function Home() {
+export async function getServerSideProps() {
+  const posts = await getPosts()
+  return { 
+    props: { posts } 
+  }
+}
+
+export default function Home(props:any) {
  
-  // async function getPosts() {
-  // const res = await fetch(
-	// 	`${process.env.NEXT_PUBLIC_apiurl}/content/posts/?key=${process.env.CONTENT_API_KEY}&fields=title,slug,custom_excerpt`
-	// ).then((res) => return res.json())
-
-	// const posts = res.posts
-
-	// return posts
-  // }
-
-fetch(`${process.env.NEXT_PUBLIC_apiurl}/content/posts/?key=${process.env.NEXT_PUBLIC_apiKEY}`)
-.then(data => {
-return data.json();
-})
+  const {posts} = props
   return (
-   <div>
-    
-   </div>
+    <div>
+      <h1>Hello to my blog</h1>
+      <ul>
+      {posts?.map((item:any, index:number) => {
+        return (
+          <li  key={index}>
+            {/* <Link href="/post/[slug]" as={`/post/${item.slug}`}> */}
+              <div>{item.title}</div>
+            {/* </Link> */}
+          </li>
+        )
+      })}
+    </ul>
+    </div>
   )
 }
